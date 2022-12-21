61fa9d68308b5214124455e936c7d7c0f215fce8
ljw971125 <ljw971125@naver.com>
2022-12-21 22:50:15 +0900

    성적 나타내기

---
+++ Start/src/Study/Sungjuk.java
@@ -0,0 +1,25 @@
+package Study;
+
+public class Sungjuk {
+int kor;
+int eng;
+int math;
+String name;
+String id;
+
+public Sungjuk(int kor, int eng, int math, String name, String id) {
+	this.kor=kor;
+	this.eng=eng;
+	this.math=math;
+	this.name=name;
+	this.id=id;
+	}
+public void info() {
+	System.out.println("국어:"+kor);
+	System.out.println("영어:"+eng);
+	System.out.println("수학:"+math);
+	System.out.println("이름:"+name);
+	System.out.println("학번:"+id);
+	System.out.println("합계:"+(kor+eng+math));
+}
+}
