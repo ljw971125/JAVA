4f24b71a104553cbd97a22683f13a96c2b9d8a4c
ljw971125 <ljw971125@naver.com>
2022-12-21 23:30:16 +0900

    성적표기

--- Start/src/Study/Sungjukmain.java
+++ Start/src/Study/Sungjukmain.java
@@ -4,7 +4,7 @@ public class Sungjukmain {
 
 	public static void main(String[] args) {
 		Sungjuk s=new Sungjuk(100,80,90,"김길동","030106");//점수기입,이름기입,학번기입
-		s.info();
+		s.info();//클래스에서 호출
 
 	}
 
