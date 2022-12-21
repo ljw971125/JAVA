3ee93af32819d7beb2605b3a12cc39fcfd8b1892
ljw971125 <ljw971125@naver.com>
2022-12-22 00:48:43 +0900

    사람이 가지는 정보

---
+++ Personproperty/src/Person.java
@@ -0,0 +1,11 @@
+public class Person{
+//사람이 가지는 속성은 무엇인가?변수
+public String name;
+public String jumin;
+
+//사람이 가지는 기능은 무엇인가?함수
+public void info(){
+System.out.println("이름:"+name);
+System.out.println("주민번호:"+jumin);
+}
+}
---
+++ Personproperty/src/PersonMain.java
@@ -0,0 +1,17 @@
+public class PersonMain{
+public static void main(String[] args){
+Person p1=new Person();
+Person p2=new Person();
+
+p1.name="홍길동";
+p1.jumin="801010-1111111";
+
+p2.name="김꽃분";
+p2.jumin="920101-2222222";
+
+p1.info();
+p2.info();
+
+
+}
+}
