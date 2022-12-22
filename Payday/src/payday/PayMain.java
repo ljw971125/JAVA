15e5486e5fab6a2ec7d010884d0128210edb9b6f
ljw971125 <ljw971125@naver.com>
2022-12-22 10:27:54 +0900

    반복문 추가,종료버튼 추가,리셋 추가

--- Payday/src/payday/PayMain.java
+++ Payday/src/payday/PayMain.java
@@ -1,33 +1,40 @@
 package payday;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 //월급 계산
 public class PayMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//스캐너를 이용하여 근무연수를 입력받아 계산하시오.
-		Scanner scan = new Scanner(System.in); //근무(경력) 년수
-		int year =scan.nextInt();
+		int sum=0;
 		int child =0; //자녀수
-		int pay = 1500000; //초봉 기본급
-
+		 //초봉 기본급
+		System.out.println("근무 연수를 입력하세요");
+		while(true){
+			int pay = 1500000;
+			Scanner scan = new Scanner(System.in); //근무(경력) 년수
+			int year =scan.nextInt();
 		if(year ==0) {//조건
 			System.out.println("신입사원입니다.");
-		}else if(year==1) {
-			pay = pay + 200000;
-			System.out.println("경력 1년 입니다.");
-		}else if(year==2) {
-			pay=pay+400000;
-		}else if(year==3) {
-			pay=pay+600000;
-		}else if(year==4) {
-			pay=pay+800000;
+		}else if((1<=year) && (3>year)){
+			pay = pay + 500000;
+		}else if((3<=year) && (5>year)) {
+			pay=pay+1000000;
+		}else if((5<=year) && (10>year)) {
+			pay=pay+1500000;
 		}else {//위의 조건에 모두 충족하지 못할때
-			pay =pay+1500000;
+			pay =pay+3000000;
 		}
-		System.out.println("당신의 급여는"+ pay +"입니다.");
-
+		
+		System.out.println("당신의 급여는"+ pay +"입니다.(종료:q)");
+		char exit=(char)System.in.read();//종료시켜줌
+		if(exit=='q') {
+			System.out.println("프로그램을 종료합니다.");
+			break;
+			}//밖으로 빠져나감
+		}
 	}
 
-}
+	}
