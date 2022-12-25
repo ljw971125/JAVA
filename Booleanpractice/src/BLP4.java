5324d1ba4a023051d0e6208f33237c756500fb78
이지운 <@59.9.88.74>
2022-12-25 17:48:09 +0900

    크기 비교 연습4

---
+++ Booleanpractice/src/BLP4.java
@@ -0,0 +1,14 @@
+
+public class BLP4 {
+//크기비교 !=,==
+	public static void main(String[] args) {
+		if(3!=4) {
+			System.out.println("두 수는 다릅니다.");
+		}
+		/*if(3==4) {
+			System.out.println("두 수는 다릅니다.");
+		}
+*/
+	}
+
+}
