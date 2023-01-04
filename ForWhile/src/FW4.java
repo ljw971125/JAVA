86fa376c204f250cb912797b5049b2dd5871af15
ljw971125 <ljw971125@naver.com>
2023-01-04 13:39:20 +0900

    While문 연습

---
+++ ForWhile/src/FW4.java
@@ -0,0 +1,18 @@
+
+public class FW4 {
+
+	public static void main(String[] args) {
+		int j=1;
+		do {
+			System.out.println("번호 : " + j); 
+			j++;
+		}while(j<3);
+		
+		System.out.println("----------END----------");
+		
+	
+		}
+
+	}
+
+
