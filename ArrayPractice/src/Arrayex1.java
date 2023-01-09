ff046fd77e661dce21dcf52564d0f3345fc30806
ljw971125 <ljw971125@naver.com>
2023-01-10 08:56:05 +0900

    배열연습1

---
+++ ArrayPractice/src/Arrayex1.java
@@ -0,0 +1,14 @@
+
+public class Arrayex1 {
+
+	public static void main(String[] args) {
+		int weight[] = new int[10];//10개의 배열을 넣을 공간을 생성
+		for(int i = 0; i<10; i++) {
+			System.out.println(i+"번째:"+weight[i]);
+		}
+
+		
+
+	}
+
+}
